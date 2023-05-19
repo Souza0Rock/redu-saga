bfd305973c13320396074fa5ea082b6affef8fdd
Souza0Rock <mateusmsouza04@gmail.com>
2023-05-19 15:09:17 -0300

    backup

---
+++ src/redux/actions/user/index.ts
@@ -0,0 +1,67 @@
+// /* eslint-disable no-labels */
+// /* eslint-disable @typescript-eslint/no-unused-expressions */
+// /* eslint-disable no-lone-blocks */
+// /* eslint-disable no-unreachable */
+// /* eslint-disable no-restricted-globals */
+// /* eslint-disable no-unused-labels */
+// import * as types from '../../types'
+
+// export type UserType = {
+//     name?: string;
+//     avatar_url?: string
+// }
+
+// export function getUserRequest(name: string) {
+//     type: string
+//     payload: string
+// } {
+//     return{
+//         type: types.GET_USER_REQUEST,
+//         payload: name
+//     }
+// }
+
+// export function getUserSuccess(user: UserType) {
+//     type: string
+//     payload: UserType
+// } {
+//     return{
+//         type: types.GET_USER_SUCCESS,
+//         payload: user
+//     }
+// }
+
+// export function GET_USER_FAILURE() {
+//     type: string
+// } {
+//     return{
+//         type: types.GET_USER_FAILURE
+//     }
+// }
+
+import * as types from '../../types';
+
+export type UserType = {
+  name?: string;
+  avatar_url?: string;
+};
+
+export function getUserRequest(name: string) {
+  return {
+    type: types.GET_USER_REQUEST,
+    payload: name,
+  };
+}
+
+export function getUserSuccess(user: UserType) {
+  return {
+    type: types.GET_USER_SUCCESS,
+    payload: user,
+  };
+}
+
+export function getUserFailure() {
+  return {
+    type: types.GET_USER_FAILURE,
+  };
+}
